74f224950ccbdec67f9c215a723b72518b9f76db
0xfabian <dumitru_fabian0@yahoo.com>
2024-09-05 03:31:50 +0300

    :)

--- test.asm
+++ test.asm
@@ -4,9 +4,9 @@ main:
     mov rax, 1  ; write
     mov rdi, 1  ; fd = stdout
     lea rsi, [rip + 25] ; address of msg
-    mov rdx, 5  ; size
+    mov rdx, 6  ; size
     syscall
     mov rax, 60 ; exit
     mov rdi, 0
     syscall
-    msg: db 70 75 6C 61 0a
+    db 68 65 6c 6c 6f 0a
